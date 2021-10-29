b0e3296e7b7df18523697d2506bcafaf92684c9a
AlfredT1999 <alfredtapia1999@outlook.com>
2021-10-28 20:59:48 -0500

    Section 11: Implement Generic Unit Of Work Pattern. Class 59: Implement Generic Repository.
    1. First we're gona modify our IRepositoryBase.cs file by adding our new interface called IGenericRepository.
    2. After that I've implemented a new class called GenericRepository inside of my Repository folder.

--- System-Management-master/Employee Management System/Employee Management System/Contracts/IRepositoryBase.cs
+++ System-Management-master/Employee Management System/Employee Management System/Contracts/IRepositoryBase.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace Employee_Management_System.Contracts
@@ -28,5 +29,29 @@ namespace Employee_Management_System.Contracts
 
         // Save:
         Task<bool> Save();
+    }
+
+    public interface IGenericRepository<T> where T : class
+    {
+        // Create:
+        Task Create(T entity);
+
+        // Read:
+        Task<IList<T>> FindAll(
+            Expression<Func<T, bool>> expression = null,// This works like q => q.Id == 20
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,// This works like q => q.OrederBy(q == q.Id)
+            List<string> includes = null// This works like a Join query.
+            );
+
+        // Add another declaration that find if the id passed exists.
+        Task<bool> isExists(Expression<Func<T, bool>> expression = null);
+
+        Task<T> Find(Expression<Func<T, bool>> expression, List<string> includes = null);
+
+        // Update:
+        void Update(T entity);
+
+        // Delete:
+        void Delete(T entity);
     }
 }
